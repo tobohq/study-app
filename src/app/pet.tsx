204054a3e2055ad7c8050c4d54d48dcb0a636cd7
tobohq <jacechanas@gmail.com>
2024-10-26 16:31:27 -0700

    Changes to Webpage

--- src/app/pet.tsx
+++ src/app/pet.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-
+import Navbar from "../components/Navbar";
 
 export default function PetPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-
+      <Navbar />
       <div className = "flex flex-wrap">
       </div>
     </main>
